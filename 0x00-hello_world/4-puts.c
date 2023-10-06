8742f08ca471da7f4468133478ea9cdb32ee289e
Salma-Hamzaoui <hamzaoui.salma@gmail.com>
2023-10-06 15:39:20 -0700

    done

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main -  Entry point
+ *
+ * Description: c program without using printf function
+ *
+ * Return: it return 0 which mean success
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
