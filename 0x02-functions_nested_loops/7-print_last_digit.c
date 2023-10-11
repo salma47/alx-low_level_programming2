2df2d59faeddd11a4a345d4e1bf7ed4f00e81999
Salma-Hamzaoui <hamzaoui.salma@gmail.com>
2023-10-11 14:33:16 -0700

    so

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include"main.h"
+
+/**
+ *  print_last_digit - print last digit
+ *
+ *  @n: integer param
+ *
+ *  Return: last digit
+*/
+
+int print_last_digit(int n)
+{
+	int last;
+
+	if (n < 0)
+		last = -1 * (n % 10);
+	else
+		last = n % 10;
+
+	_putchar(last + '0');
+	return (last);
+}
